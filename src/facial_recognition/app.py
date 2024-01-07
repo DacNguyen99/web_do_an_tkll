ff6c24c9b4dc3c2771d638be08e2b9ed68a3e6cd
Kov1103 <132475227+Kov1103@users.noreply.github.com>
2024-01-08 05:16:05 +0700

    Add files via upload

---
+++ src/facial_recognition/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+import subprocess
+import logging
+
+# Thiết lập cấu hình logging
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
+app = Flask(__name__)
+
+@app.route('/run_face_rec', methods=['GET'])
+def run_face_rec():
+    try:
+        # Nhận input từ request (nếu có)
+        input_data = request.args.get('input_data')
+        logging.info(f'Received input_data: {input_data}')
+
+        result = subprocess.run(['python', 'facial_req.py'], stdout=subprocess.PIPE, text=True)
+        output = result.stdout
+
+        logging.info(f'Output: {output}')
+
+        return jsonify({'output': output})
+    except Exception as e:
+        logging.error(f'Error running face_rec.py: {str(e)}')
+        return jsonify({'error': str(e)})
+    
+if __name__ == '__main__':
+    app.run(debug=True)
